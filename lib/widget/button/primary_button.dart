217fb75e52bda45bc097f57bbd8c83d6772774ae
Maharta <gedemaharta650@gmail.com>
2021-12-01 10:06:14 +0800

    Renamed BlackButton to PrimaryButton and adjusted code.

---
+++ lib/widget/button/primary_button.dart
@@ -0,0 +1,27 @@
+import 'package:berikan/common/style.dart';
+import 'package:flutter/material.dart';
+
+class PrimaryButton extends StatelessWidget {
+  final String text;
+  final Function() onPressed;
+  const PrimaryButton({Key? key, required this.text, required this.onPressed}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 67, vertical: 15),
+        child: Text(
+            text,
+          style: Theme.of(context).textTheme.button,
+        ),
+      ),
+      style: ElevatedButton.styleFrom(
+        elevation: 10,
+        primary: colorPrimaryDark,
+        shape: const StadiumBorder(),
+      ),
+    );
+  }
+}
