bbd1c76902f9b2543180fcb5dfc08a995166a0f6
Maharta <gedemaharta650@gmail.com>
2021-12-05 23:55:49 +0800

    added new dummy constant and style for development

--- lib/common/style.dart
+++ lib/common/style.dart
@@ -6,6 +6,29 @@ import 'package:google_fonts/google_fonts.dart';
 const gradientSecondaryPrimaryStart = Color(0xFFD2FBE7);
 const gradientSecondaryPrimaryEnd = Color(0xFF4FB3BF);
 const colorPrimaryDark = Color(0xFF005662);
+const colorPrimary = Color(0xFF00838F);
+
+
+ThemeData lightTheme = ThemeData(
+  appBarTheme: AppBarTheme(
+    iconTheme: const IconThemeData(
+      color: Colors.black
+    ),
+    backgroundColor: Colors.white,
+    titleTextStyle: textTheme.headline6!.copyWith(
+      color: Colors.black
+    ),
+  ),
+  primaryColor: colorPrimary,
+  scaffoldBackgroundColor: Colors.white,
+  visualDensity: VisualDensity.adaptivePlatformDensity,
+  textTheme: textTheme,
+  snackBarTheme: const SnackBarThemeData(
+    actionTextColor: Colors.orange,
+  ),
+  floatingActionButtonTheme:
+  const FloatingActionButtonThemeData(backgroundColor: colorPrimary),
+);
 
 final blackTitle = GoogleFonts.roboto(
     color: Colors.black,
@@ -38,7 +61,7 @@ final textTheme = TextTheme(
       fontWeight: FontWeight.w400
   ),
   headline6: GoogleFonts.roboto(
-      fontSize: 32,
+      fontSize: 20,
       fontWeight: FontWeight.w500,
       letterSpacing: 0.15
   ),
