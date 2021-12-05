0fad7a71795e0a6becba54adb7b4887201cf1643
Maharta <gedemaharta650@gmail.com>
2021-12-05 23:58:09 +0800

    refactor -> outlinedButton to a custom widget.

---
+++ lib/widget/button/custom_outlinedbutton.dart
@@ -0,0 +1,34 @@
+import 'package:berikan/common/style.dart';
+import 'package:berikan/ui/signup_page.dart';
+import 'package:flutter/material.dart';
+
+class CustomOutlinedButton extends StatelessWidget {
+  final String text;
+
+  const CustomOutlinedButton({Key? key, required this.text}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      child: SizedBox(
+        width: 300,
+        height: 72,
+        child: OutlinedButton(
+          style: OutlinedButton.styleFrom(
+              shape: const StadiumBorder(),
+              side: const BorderSide(width: 5, color: colorPrimaryDark)),
+          onPressed: () {
+            Navigator.pushNamed(context, SignupPage.routeName);
+          },
+          child: Text(
+            text,
+            style: Theme.of(context)
+                .textTheme
+                .button!
+                .copyWith(color: colorPrimaryDark),
+          ),
+        ),
+      ),
+    );
+  }
+}
