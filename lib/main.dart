789d4fcdecef7975e1bd96694bfce41fa3351263
Maharta <gedemaharta650@gmail.com>
2021-12-06 00:37:59 +0800

    added route for new UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:berikan/common/style.dart';
 import 'package:berikan/ui/add_item_page.dart';
+import 'package:berikan/ui/chat_page.dart';
 import 'package:berikan/ui/home_page.dart';
 import 'package:berikan/ui/login_page.dart';
 import 'package:berikan/ui/main_page.dart';
@@ -29,6 +30,7 @@ class MyApp extends StatelessWidget {
         HomePage.routeName: (context) => const HomePage(),
         LoginPage.routeName: (context) => const LoginPage(),
         AddItemPage.routeName: (context) => const AddItemPage(),
+        ChatPage.routeName: (context) => const ChatPage(),
       },
     );
   }
