45fd97d63a5aff6cd400ccef67e5b6673a0bf3bc
Maharta <gedemaharta650@gmail.com>
2021-12-08 23:49:47 +0800

    added a dummyChat data for chat detail page.s

--- lib/common/constant.dart
+++ lib/common/constant.dart
@@ -1,6 +1,29 @@
 const lorem =
     'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed molestie velit et augue laoreet, blandit semper dui ornare. Etiam eu egestas sapien.';
 
-final List<Map> myProducts =
-List.generate(100, (index) => {"id": index, "name": "Product $index", })
-    .toList();
+final List<Map> myProducts = List.generate(
+    100,
+    (index) => {
+          "id": index,
+          "name": "Product $index",
+        }).toList();
+
+final List<Map<String, dynamic>> dummyChat2 = [
+  {
+    'sender': 'Me',
+    'message': 'Halo, apakah barang ini masih ada ya?'
+  },
+  {
+    'sender': 'John',
+    'message': 'Masih ada, silahkan langsung ambil kesini ya!'
+  },
+  {
+    'sender': 'Me',
+    'message': 'Oke, aku otw sekarang'
+  },
+  {
+    'sender': 'John',
+    'message': 'Oke, ditunggu di depan.'
+  },
+
+];
