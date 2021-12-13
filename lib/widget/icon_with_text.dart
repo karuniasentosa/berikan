c521b058d579c0323dc9f4a629b9bbb909ff1014
Karunia Sentosa <s32190083@student.ubm.ac.id>
2021-12-13 13:19:33 +0700

    added widget icon with text

---
+++ lib/widget/icon_with_text.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class IconWithText extends StatelessWidget {
+  final Icon icon;
+  final String text;
+  final TextStyle? textStyle;
+
+  IconWithText({required this.icon, required this.text, this.textStyle});
+
+  @override
+  Widget build(BuildContext context) {
+    return Flexible(
+      child: Row(
+          children: <Widget>[
+            icon,
+            const SizedBox(width: 4),
+            Flexible(
+              child: Text(
+              text,
+              style: textStyle,
+              maxLines: 2,
+            ),
+          )
+        ]
+      ),
+    );
+  }
+}
