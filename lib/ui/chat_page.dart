b906466f645bd036047ae1f98beb240637c8c11e
Karunia Sentosa <s32190083@student.ubm.ac.id>
2021-12-15 12:59:23 +0700

    Glued Chat Page UI with the logic.

--- lib/ui/chat_page.dart
+++ lib/ui/chat_page.dart
@@ -1,6 +1,9 @@
+import 'package:berikan/provider/chat_page_provider.dart';
+import 'package:berikan/provider/provider_result_state.dart';
 import 'package:berikan/ui/chat_detail_page.dart';
 import 'package:berikan/utills/arguments.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class ChatPage extends StatelessWidget {
   static const routeName = '/chatPage';
@@ -9,6 +12,7 @@ class ChatPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Provider.of<ChatPageProvider>(context, listen: false).getMyChats();
     return Scaffold(
       appBar: AppBar(
         title: const Text('Chat'),
@@ -31,26 +35,44 @@ class ChatPage extends StatelessWidget {
             ]),
           ),
           Expanded(
-            child: ListView.builder(
-              itemBuilder: (context, index) {
-                return Card(
-                  child: ListTile(
-                    onTap: () {
-                      Navigator.pushNamed(context, ChatDetailPage.routeName,
-                          arguments: Arguments('John Doe',
-                              'https://asset.kompas.com/crops/1g9P4L73NLmOshdRUptmBe_oQgQ=/0x0:698x465/750x500/data/photo/2020/12/07/5fce3837c4f6d.jpg'));
+            child: Consumer<ChatPageProvider>(
+              builder: (context, provider, widget) {
+                if (provider.state == ProviderResultState.hasData) {
+                  final chatDatas = provider.chats;
+                  return ListView.builder(
+                    itemBuilder: (context, index) {
+                      return Card(
+                        child: ListTile(
+                          onTap: () {
+                            Navigator.pushNamed(
+                                context, ChatDetailPage.routeName,
+                                arguments: Arguments('John Doe',
+                                    'https://asset.kompas.com/crops/1g9P4L73NLmOshdRUptmBe_oQgQ=/0x0:698x465/750x500/data/photo/2020/12/07/5fce3837c4f6d.jpg'));
+                          },
+                          leading: ClipRRect(
+                            borderRadius: BorderRadius.circular(50),
+                            child: Image.memory(chatDatas[index].imageData),
+                          ),
+                          title: Text(chatDatas[index].name),
+                          subtitle: Text(chatDatas[index].lastMessage),
+                        ),
+                      );
                     },
-                    leading: ClipRRect(
-                      borderRadius: BorderRadius.circular(50),
-                      child: Image.network(
-                          'https://asset.kompas.com/crops/1g9P4L73NLmOshdRUptmBe_oQgQ=/0x0:698x465/750x500/data/photo/2020/12/07/5fce3837c4f6d.jpg'),
-                    ),
-                    title: Text('John Doe'),
-                    subtitle: Text('Siap, saya segera kesana!'),
-                  ),
-                );
+                    itemCount: chatDatas.length,
+                  );
+                } else if (provider.state == ProviderResultState.noData) {
+                  // TODO: Add no chat widget(?)
+                  return Text('no chat');
+                } else if (provider.state == ProviderResultState.loading) {
+                  return CircularProgressIndicator();
+                } else if (provider.state == ProviderResultState.error) {
+                  // TODO: Add error widget(?)
+                  return Text(provider.errorMessage);
+                } else {
+                  // TODO: handle no connectyion
+                  return Placeholder();
+                }
               },
-              itemCount: 8,
             ),
           ),
         ],
