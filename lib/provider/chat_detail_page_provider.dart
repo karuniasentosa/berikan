61ce7b222b90845ad778dd9e9283fbcf1300ebf5
Karunia Sentosa <s32190083@student.ubm.ac.id>
2021-12-16 19:00:03 +0700

    Implemented logic on Chat Detail Page.

---
+++ lib/provider/chat_detail_page_provider.dart
@@ -0,0 +1,61 @@
+import 'dart:async';
+
+import 'package:berikan/api/model/chat.dart';
+import 'package:berikan/api/model/extensions/chat_extensions.dart';
+import 'package:berikan/api/model/message.dart';
+import 'package:berikan/provider/provider_result_state.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class ChatDetailPageProvider extends ChangeNotifier
+{
+  late final Chat _chatReference;
+  ProviderResultState _state = ProviderResultState.noData;
+  late Stream<List<Message>> _messageListStream;
+  String _errorMessage = "";
+
+  ProviderResultState get state => _state;
+  Stream<List<Message>> get messages => _messageListStream;
+  String get errorMessage => _errorMessage;
+
+  Future _getMessage(Chat chat) async
+  {
+    try {
+      _messageListStream = _chatReference.messages.transform(_MyStreamTransformer());
+      _state = ProviderResultState.hasData;
+    } catch (e) {
+      _state = ProviderResultState.error;
+      _errorMessage = e.toString();
+      rethrow;
+    }
+  }
+
+  void getMessage(Chat chat) {
+    _chatReference = chat;
+    _state = ProviderResultState.loading;
+    notifyListeners();
+    _getMessage(chat);
+  }
+}
+
+// grabbed from https://stackoverflow.com/a/63982248/
+class _MyStreamTransformer implements StreamTransformer<QuerySnapshot<Message>, List<Message>>
+{
+  final StreamController<List<Message>> _controller = StreamController();
+  StreamSubscription<QuerySnapshot<Message>>? streamSubscription;
+
+  @override
+  Stream<List<Message>> bind(Stream<QuerySnapshot<Message>> stream) {
+    streamSubscription = stream.listen((event) {
+      final qdsList = event.docs;
+      _controller.add(qdsList.map((e) => e.data()).toList());
+    });
+
+    return _controller.stream;
+  }
+
+  @override
+  StreamTransformer<RS, RT> cast<RS, RT>() {
+    return StreamTransformer.castFrom(this);
+  }
+}
--- lib/ui/chat_detail_page.dart
+++ lib/ui/chat_detail_page.dart
@@ -1,18 +1,34 @@
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:berikan/api/account_service.dart';
+import 'package:berikan/api/model/chat.dart';
+import 'package:berikan/api/model/extensions/chat_extensions.dart';
+import 'package:berikan/api/model/chat_data.dart';
+import 'package:berikan/api/model/message.dart';
+import 'package:berikan/api/storage_service.dart';
 import 'package:berikan/common/style.dart';
-import 'package:berikan/utills/arguments.dart';
+import 'package:berikan/provider/chat_detail_page_provider.dart';
+import 'package:berikan/provider/provider_result_state.dart';
 import 'package:berikan/widget/message_bubble.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
-import 'package:berikan/common/constant.dart';
+import 'package:provider/provider.dart';
 
 class ChatDetailPage extends StatelessWidget {
   static const routeName = '/chatDetailPage';
 
-  const ChatDetailPage({Key? key}) : super(key: key);
+  final Chat chat;
+  final _textEditingController = TextEditingController();
+  final _chatScrollingController = ScrollController();
+  File? imageFile;
+
+  ChatDetailPage({Key? key, required this.chat}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final args = ModalRoute.of(context)?.settings.arguments as Arguments;
-
+    Provider.of<ChatDetailPageProvider>(context, listen: false)
+        .getMessage(chat);
     return Scaffold(
         appBar: AppBar(
           title: Row(
@@ -21,27 +37,91 @@ class ChatDetailPage extends StatelessWidget {
                 backgroundColor: Colors.transparent,
                 child: ClipRRect(
                   borderRadius: BorderRadius.circular(15),
-                  child: Image.network(args.imageUrl),
+                  child: FutureBuilder<ChatData>(
+                      future: ChatData.of(chat),
+                      builder: (context, AsyncSnapshot<ChatData> snapshot) {
+                        if (snapshot.hasData) {
+                          return Image.memory(snapshot.data!.imageData);
+                        } else {
+                          return Container(color: Colors.grey);
+                        }
+                      }),
                 ),
               ),
               SizedBox(
                 width: 12,
               ),
-              Text(args.name),
+              FutureBuilder<ChatData>(
+                  future: ChatData.of(chat),
+                  builder: (context, AsyncSnapshot<ChatData> snapshot) {
+                    if (snapshot.hasData) {
+                      return Text(snapshot.data!.name);
+                    } else {
+                      return Text('...');
+                    }
+                  }),
             ],
           ),
         ),
         body: Column(
           children: [
             Expanded(
-              child: ListView.builder(
-                itemCount: dummyChat2.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return MessageBubble(sender: dummyChat2[index]['sender'], text: dummyChat2[index]['message'], isMyChat: dummyChat2[index]['sender'] == 'Me');
+              child: Consumer<ChatDetailPageProvider>(
+                builder: (context, provider, _) {
+                  if (provider.state == ProviderResultState.loading) {
+                    return CircularProgressIndicator();
+                  } else if (provider.state == ProviderResultState.noData) {
+                    return Text('no data');
+                  } else if (provider.state == ProviderResultState.hasData) {
+                    return StreamBuilder<List<Message>>(
+                      stream: provider.messages,
+                      builder: (context, snapshot) {
+                        if (snapshot.hasData) {
+                          final messageList = snapshot.requireData;
+                          return ListView.builder(
+                            controller: _chatScrollingController,
+                            itemCount: messageList.length,
+                            itemBuilder: (BuildContext context, int index) {
+                              final message = messageList[index];
+                              if (message.attachment != null) {
+                                // TODO: bagian ini jelek banget ::(
+                                return FutureBuilder<Uint8List?>(
+                                    future: StorageService.getData(FirebaseStorage.instance.ref(message.attachment!)),
+                                    builder: (context, snapshot) {
+                                      if (snapshot.hasData) {
+                                        return MessageBubble.attachment(
+                                            time: TimeOfDay.fromDateTime(message.when),
+                                            imageFile: snapshot.requireData!,
+                                            isMyChat: AccountService.getCurrentUser()!.uid == message.accountId);
+                                      } else {
+                                          return Placeholder();
+                                      }
+                                    }
+                                );
+                              } else {
+                                return MessageBubble.text(
+                                    time: TimeOfDay.fromDateTime(message.when),
+                                    text: message.content!,
+                                    isMyChat:
+                                        AccountService.getCurrentUser()!.uid ==
+                                            message.accountId);
+                              }
+                            },
+                          );
+                        } else {
+                          return CircularProgressIndicator();
+                        }
+                      },
+                    );
+                  } else {
+                    return Text('error: ${provider.errorMessage}');
+                  }
                 },
               ),
             ),
-            const SizedBox(height: 8,),
+            const SizedBox(
+              height: 8,
+            ),
             Row(
               children: [
                 Padding(
@@ -54,13 +134,18 @@ class ChatDetailPage extends StatelessWidget {
                       color: colorPrimary,
                     ),
                     child: IconButton(
-                      icon: const Icon(Icons.photo_camera_outlined, color: Colors.white), onPressed: () {  },
+                      icon: const Icon(Icons.photo_camera_outlined,
+                          color: Colors.white),
+                      onPressed: () {
+
+                      },
                     ),
                   ),
                 ),
                 Expanded(
                   child: Padding(
-                    padding: const EdgeInsets.only(right: 8.0, top: 8, bottom: 8),
+                    padding:
+                        const EdgeInsets.only(right: 8.0, top: 8, bottom: 8),
                     child: TextField(
                       decoration: InputDecoration(
                         contentPadding: const EdgeInsets.all(8.0),
@@ -68,9 +153,9 @@ class ChatDetailPage extends StatelessWidget {
                           borderRadius: BorderRadius.circular(30),
                         ),
                       ),
+                      controller: _textEditingController,
                     ),
                   ),
-
                 ),
                 Padding(
                   padding: const EdgeInsets.all(4.0),
@@ -78,19 +163,42 @@ class ChatDetailPage extends StatelessWidget {
                     height: 40,
                     width: 50,
                     decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: colorPrimary
+                        shape: BoxShape.circle,
+                        color: colorPrimary
                     ),
                     child: IconButton(
-                      icon: const Icon(Icons.send, color: Colors.white,), onPressed: () {  },
+                      disabledColor: Colors.grey,
+                      icon: const Icon(
+                        Icons.send,
+                        color: Colors.white,
+                      ),
+                      onPressed: _sendMessage,
                     ),
                   ),
                 ),
               ],
             ),
-
-
           ],
-        ));
+        )
+    );
+  }
+
+  void _sendMessage() async {
+    if (_textEditingController.text.isNotEmpty) {
+      final message = Message.text(
+          accountId: AccountService.getCurrentUser()!.uid,
+          content: _textEditingController.text
+      );
+      chat.pushMessage(message);
+      _textEditingController.clear();
+
+      Future.delayed(Duration(milliseconds: 500), () {
+        _chatScrollingController.animateTo(
+            _chatScrollingController.position.maxScrollExtent,
+            duration: Duration(milliseconds: 200),
+            curve: Curves.easeIn
+        );
+      });
+    }
   }
 }
