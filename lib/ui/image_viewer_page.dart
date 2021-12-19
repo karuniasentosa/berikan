f5920439cb5c7e3fca842672c448c20332022701
Karunia Sentosa <s32190083@student.ubm.ac.id>
2021-12-19 21:16:59 +0700

    added image viewer page.

---
+++ lib/ui/image_viewer_page.dart
@@ -0,0 +1,41 @@
+import 'dart:typed_data';
+
+import 'package:berikan/widget/button/primary_button.dart';
+import 'package:flutter/material.dart';
+
+class ImageViewerPage extends StatelessWidget
+{
+  static const routeName = '/image_viewer_page';
+
+  final String imageId;
+  final Uint8List imageData;
+
+  const ImageViewerPage({Key? key, required this.imageId, required this.imageData}): super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: Icon(Icons.arrow_back, color: Colors.white)
+        ),
+      ),
+      body: Stack(
+        children: [
+          Hero(
+            tag: imageId,
+            child: Image.memory(imageData),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: PrimaryButton(text: 'Simpan', onPressed: (){}),
+          )
+        ],
+      ),
+    );
+  }
+
+}
