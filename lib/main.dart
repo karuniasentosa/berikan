3e32f6e467bae6301d1d9992ddd745250159bde0
Karunia Sentosa <s32190083@student.ubm.ac.id>
2021-12-19 21:17:14 +0700

    added route for image viewer page.

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,12 @@ import 'package:berikan/ui/add_item_page.dart';
 import 'package:berikan/ui/chat_detail_page.dart';
 import 'package:berikan/ui/chat_page.dart';
 import 'package:berikan/ui/home_page.dart';
+import 'package:berikan/ui/image_viewer_page.dart';
 import 'package:berikan/ui/login_page.dart';
 import 'package:berikan/ui/main_page.dart';
 import 'package:berikan/ui/signup_continue_page.dart';
 import 'package:berikan/ui/signup_page.dart';
+import 'package:berikan/utills/arguments.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'common/style.dart';
 
@@ -36,6 +38,10 @@ class MyApp extends StatelessWidget {
         AddItemPage.routeName: (context) => AddItemPage(),
         ChatPage.routeName: (context) => const ChatPage(),
         ChatDetailPage.routeName: (context) => ChatDetailPage(),
+        ImageViewerPage.routeName: (context) {
+          final arguments = ModalRoute.of(context)?.settings.arguments as ImageViewerArguments;
+          return ImageViewerPage(imageId: arguments.imageId, imageData: arguments.imageData);
+        }
       },
     );
   }
