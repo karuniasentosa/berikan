b647aff9ecf46fe888b97c581cdd649716c3c0a7
Maharta <gedemaharta650@gmail.com>
2021-12-20 12:52:14 +0800

    added getAccountById and getLocation function.

--- lib/api/account_service.dart
+++ lib/api/account_service.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 import 'model/account.dart';
 
@@ -24,8 +25,42 @@ class AccountService {
     return (await docRef.get()).data();
   }
 
+  static Future<DocumentSnapshot<Map<String, dynamic>>> getAccountById(String ownerId) async{
+    /// Not using accountDocumentReference since it's using converter without location.
+    final docRef = FirebaseFirestore.instance.collection(Account.collectionName).doc(ownerId);
+    final data = await docRef.get();
+    return data;
+  }
+
   static Future<void> addAccount(String? id,Account account) async {
     accountDocumentReference(FirebaseFirestore.instance, id!).set(account);
+  }
+
+  static Future<List<dynamic>> getLocation(String ownerId) async {
+    final accountDocRef = FirebaseFirestore.instance
+        .collection('account').doc(ownerId);
+    final snapshot = await accountDocRef.get();
+
+    final location = snapshot.data()!['location'] as List<dynamic>;
+
+    return location;
+  }
+
+  static Future<dynamic> getProfilePicture(String ownerId) async {
+    final accountDocRef = FirebaseFirestore.instance.collection(Account.collectionName).doc(ownerId);
+    final snapshot = await accountDocRef.get();
+    final String profileImage = snapshot.data()!['avatar_url'];
+
+    if(profileImage.length>0){
+      final imageRef = FirebaseStorage.instance.ref(profileImage);
+
+      return imageRef;
+    } else {
+      return 'This user don\'t have a profile picture yet';
+    }
+
+
+
   }
 
   static Future<void> signIn(String email, String password) async {
