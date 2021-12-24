0ce2a4a108291f6784526bc842a681f6a95d9705
karuniasentosa <82386912+karuniasentosa@users.noreply.github.com>
2021-12-24 12:11:41 +0700

    BUGFIX: Removes duplication in chat page. (#26)
    * Added function.
    
    * Modified "Chat Penjual" button. Making it easier to read.

--- lib/api/chat_service.dart
+++ lib/api/chat_service.dart
@@ -30,6 +30,36 @@ class ChatService
     return snapshot.docs.map((e) => e.data()).toList();
   }
 
+  /// Gets a chat between two endpoints.
+  /// If there doesn't exists, it will create a new one.
+  static Future<Chat> getChatFromEndpoints({required String endpointUid1, required String endpointUid2}) async {
+    final instance = FirebaseFirestore.instance;
+
+    final colRef = chatCollectionReference(instance);
+
+    final query = colRef.where(
+        'endpoints',
+        whereIn: [
+          [accountDocumentReference(instance, endpointUid1), accountDocumentReference(instance, endpointUid2)],
+          [accountDocumentReference(instance, endpointUid2), accountDocumentReference(instance, endpointUid1)],
+        ]).limit(1);
+
+    final snapshot = await query.get();
+
+    if (snapshot.size == 0) {
+      // create a chat
+      final docRef = await colRef.add(
+          Chat.create(
+              endpointAccountId1: endpointUid1,
+              endpointAccountId2: endpointUid2)
+      );
+      final snapshot = await docRef.get();
+      return await snapshot.data()!;
+    } else {
+      return snapshot.docs[0].data();
+    }
+  }
+
   static Future<Chat> createChatWith(String userId) async {
     final uid = AccountService.getCurrentUser()!.uid;
 
